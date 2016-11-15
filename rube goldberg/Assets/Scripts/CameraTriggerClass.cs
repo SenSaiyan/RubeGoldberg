b72e0aff8b9bce084dca46c8a9e82242e1f519ff
Daniel Jiang <djj286@nyu.edu>
2016-11-15 14:25:43 -0500

    2. YEEEEEET

---
+++ rube goldberg/Assets/Scripts/CameraTriggerClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTriggerClass : MonoBehaviour {
+	//public GameObject camToActivate;
+	public int camNumber;
+
+	public NewBehaviourScript controller;
+
+	void OnTriggerEnter(Collider other){
+
+		controller.DeactivateCameras();
+		controller.activateCamera(camNumber);
+
+	}
+}
